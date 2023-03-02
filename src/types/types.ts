2ec6c698a9c2e243471047b76a604ec1f1e7825a
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2023-03-02 12:58:33 -0400

    +updates

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,7 @@
+import { Request, Response } from "express";
+
 export type route = {
   route: string;
-  body: Function;
+  body: (request: Request, response: Response) => Promise<any>;
+  method: "GET" | "POST" | "PUT" | "DELETE";
 };
